5097fd2c14a04b3d82b4ca05c1a3ce48d9c45ac1
Futsum Nemariam <futsumnemariam2@gmil.com>
2020-10-14 13:07:52 -0700

    page commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
-import App from './Pages/App/App';
+import App from './pages/App/App';
 import * as serviceWorker from './serviceWorker';
 
 
